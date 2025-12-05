ecee48fb12a320d0f9185361fedd1c30f4cfd833
Niraj Kumar <kumarniraj8607@gmail.com>
2025-12-05 22:54:18 +0530

    Vote

--- Basic/Vote.java
+++ Basic/Vote.java
@@ -1,5 +1,14 @@
-package Basic;
-
+import java.util.*;
 public class Vote {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+        if(age >= 18){
+            System.out.println("You are eligible for vote!");
+        }else{
+            System.out.println("You are not eligible for vote!");
+        }
+        sc.close();
+    }
 }
