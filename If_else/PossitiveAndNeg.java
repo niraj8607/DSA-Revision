1a81f89c6e45feac4f1d991f03f4f4d5423337bd
Niraj Kumar <kumarniraj8607@gmail.com>
2026-01-01 00:26:37 +0530

    first commit

---
+++ If_else/PossitiveAndNeg.java
@@ -0,0 +1,17 @@
+//Write a program that takes an integer and checks whether the number is positive, negative, or zero.
+import java.util.*;
+public class PossitiveAndNeg{
+    public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter the number: ");
+      int n = sc.nextInt();
+      if(n == 0){
+        System.out.println("Number is zero : " + n);
+      }else if(n > 0){
+        System.out.println("Number is positive: "+n);
+      }else{
+        System.out.println("Number is negative");
+      }
+      sc.close();
+    }
+}
